e50e49642e734303afe54d82ebfb6b7db8b2a6e2
spirosmastorakis <spiros.mastorakis@gmail.com>
2016-05-29 00:28:27 -0700

    src/Interest: Adding some accessor methods

--- src/interest.js
+++ src/interest.js
@@ -3,8 +3,17 @@ class Interest {
     this.name = new Name(name);
     this.lifetime = 2;
   };
+
   constructInterestWithLifetime(name, lifetime) {
     this.name = new Name(name);
     this.lifetime = lifetime;
   };
+
+  getPrefix() {
+    return this.name;
+  };
+
+  getLifeTime() {
+    return this.lifetime;
+  }
 }
