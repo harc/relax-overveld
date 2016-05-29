eed0ae2fc0bd7ef0c0e77d289161e9cec8a2c236
spirosmastorakis <spiros.mastorakis@gmail.com>
2016-05-29 00:22:53 -0700

    Make data class more generic

--- src/data.js
+++ src/data.js
@@ -1,5 +1,23 @@
 class Data {
   constructor(name) {
     this.name = new Name(name);
+    this.signature = "";
+    this.content = "";
+  };
+
+  constructDataWithSig(name, signature) {
+    this.name = new Name(name);
+    this.signature = signature;
+    this.content = "";
+  };
+  constructDataWithContent(name, content) {
+    this.name = new Name(name);
+    this.signature = "";
+    this.content = content;
+  };
+  constructDataWithSigAndContent(name, signature, content) {
+    this.name = new Name(name);
+    this.signature = signature;
+    this.content = content;
   };
 }
