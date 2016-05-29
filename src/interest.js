19261b3012d4c10de71767df0e3f4b2d767b36fe
spirosmastorakis <spiros.mastorakis@gmail.com>
2016-05-29 00:23:09 -0700

    Make Interest class more generic

--- src/interest.js
+++ src/interest.js
@@ -1,5 +1,10 @@
 class Interest {
   constructor(name) {
     this.name = new Name(name);
+    this.lifetime = 2;
+  };
+  constructInterestWithLifetime(name, lifetime) {
+    this.name = new Name(name);
+    this.lifetime = lifetime;
   };
 }
