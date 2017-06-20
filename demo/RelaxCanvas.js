b7151b8c8fbc7dfc218413584b379ab8166c8bf8
Marko Röder <m.roeder@photon-software.de>
2017-06-20 16:50:13 -0700

    Made canvas scrollable (infinite in x and y direction)

--- demo/RelaxCanvas.js
+++ demo/RelaxCanvas.js
@@ -10,6 +10,9 @@ Line.prototype.involvesPoint = function(p) {
 // -----------------------------------------------------
 
 function RelaxCanvas(relax, canvas) {
+  this.offsetX = 0;
+  this.offsetY = 0;
+
   this.relax = relax;
 
   this.showConstraints = false;
@@ -89,6 +92,18 @@ RelaxCanvas.prototype.initCanvas = function(canvas) {
   this.ctxt.shadowBlur = 1;
 };
 
+RelaxCanvas.prototype.pan = function(diffX, diffY) {
+  diffX = parseInt(diffX);
+  diffY = parseInt(diffY);
+
+  if (!isNaN(diffX)) {
+    this.offsetX += diffX;
+  }
+  if (!isNaN(diffY)) {
+    this.offsetY += diffY;
+  }
+};
+
 // -----------------------------------------------------
 
 RelaxCanvas.prototype.keydown = function(k) {
@@ -144,7 +159,7 @@ RelaxCanvas.prototype.clearSelection = function() {
 
 RelaxCanvas.prototype.pointContains = function(p, x, y) {
   function square(x) { return x * x; }
-  return square(this.pointRadius) >= square(x - p.x) + square(y - p.y);
+  return square(this.pointRadius) >= square(x - p.x - this.offsetX) + square(y - p.y - this.offsetY);
 };
 
 RelaxCanvas.prototype.pointerdown = function(e) {
@@ -165,7 +180,7 @@ RelaxCanvas.prototype.pointerdown = function(e) {
       this.points.splice(pointIdx, 1);
       this.points.push(point);
       this.fingers[e.pointerId] =
-	{ x: e.clientX, y: e.clientY, point: point, constraint: constraint };
+        { x: e.clientX - this.offsetX, y: e.clientY - this.offsetY, point: point, constraint: constraint };
       point.isSelected = true;
       if (this.pointMode) {
         var oldLastPoint = this.lastPoint;
@@ -185,26 +200,40 @@ RelaxCanvas.prototype.pointerdown = function(e) {
     }
   } else if (this.pointMode) {
     var oldLastPoint = this.lastPoint;
-    this.lastPoint = this.addPoint(e.clientX, e.clientY);
+    this.lastPoint = this.addPoint(e.clientX - this.offsetX, e.clientY - this.offsetY);
     if (oldLastPoint) {
       this.addLine(oldLastPoint, this.lastPoint);
     }
+  } else { // panning
+    this.fingers[e.pointerId] = { x: e.clientX, y: e.clientY};
   }
 };
 
 RelaxCanvas.prototype.pointermove = function(e) {
   var finger = this.fingers[e.pointerId];
   if (finger) {
-    finger.x = e.clientX;
-    finger.y = e.clientY;
+    if (!finger.point) { // panning
+      var diffX = finger.x - e.clientX;
+      var diffY = finger.y - e.clientY;
+      if (diffX !== 0 || diffY != 0) {
+        this.pan(diffX, diffY);
+      }
+      finger.x = e.clientX;
+      finger.y = e.clientY;
+    } else {
+      finger.x = e.clientX - this.offsetX;
+      finger.y = e.clientY - this.offsetY;
+    }
   }
 };
 
 RelaxCanvas.prototype.pointerup = function(e) {
   var finger = this.fingers[e.pointerId];
   if (finger) {
-    finger.point.isSelected = false;
-    this.removeConstraint(finger.constraint);
+    if (finger.point) {
+      finger.point.isSelected = false;
+      this.removeConstraint(finger.constraint);
+    }
     delete this.fingers[e.pointerId];
   }
 };
@@ -258,7 +287,7 @@ RelaxCanvas.prototype.resume = function() {
 RelaxCanvas.prototype.drawPoint = function(p) {
   this.ctxt.fillStyle = p.isSelected ? 'yellow' : p.color;
   this.ctxt.beginPath();
-  this.ctxt.arc(p.x, p.y, this.pointRadius, 0, 2 * Math.PI);
+  this.ctxt.arc(p.x + this.offsetX, p.y + this.offsetY, this.pointRadius, 0, 2 * Math.PI);
   this.ctxt.closePath()
   this.ctxt.fill();
   if (p.selectionIndices.length > 0) {
@@ -272,33 +301,35 @@ RelaxCanvas.prototype.drawSelectionIndices = function(p) {
   this.ctxt.textBaseline = 'middle';
   this.ctxt.lineWidth = 1;
   this.ctxt.strokeStyle = 'blue';
-  this.ctxt.strokeText(text, p.x - 1, p.y - 1);
+  this.ctxt.strokeText(text, p.x + this.offsetX - 1, p.y + this.offsetY - 1);
   this.ctxt.stroke();
   this.ctxt.strokeStyle = 'yellow';
-  this.ctxt.strokeText(text, p.x, p.y);
+  this.ctxt.strokeText(text, p.x + this.offsetX, p.y + this.offsetY);
   this.ctxt.stroke();
 };
 
 RelaxCanvas.prototype.drawLine = function(l) {
   this.ctxt.beginPath();
-  this.ctxt.moveTo(l.p1.x, l.p1.y);
+  this.ctxt.moveTo(l.p1.x + this.offsetX, l.p1.y + this.offsetY);
   this.ctxt.lineWidth = 3;
   this.ctxt.strokeStyle = 'rgba(0,0,0,0.15)';
-  this.ctxt.lineTo(l.p2.x, l.p2.y);
+  this.ctxt.lineTo(l.p2.x + this.offsetX, l.p2.y + this.offsetY);
   this.ctxt.closePath();
   this.ctxt.stroke();
 };
 
-Relax.geom.CoordinateConstraint.prototype.draw = function(ctxt, rc) {
+Relax.geom.CoordinateConstraint.prototype.draw = function(canvas, rc) {
+  var ctxt = canvas.ctxt;
   if (this.p.isSelected) return; // don't draw over the selection highlight
   ctxt.fillStyle = 'black';
   ctxt.beginPath();
-  ctxt.arc(this.c.x, this.c.y, rc.pointRadius * 0.666, 0, 2 * Math.PI);
+  ctxt.arc(this.c.x + canvas.offsetX, this.c.y + canvas.offsetY, rc.pointRadius * 0.666, 0, 2 * Math.PI);
   ctxt.closePath();
   ctxt.fill();
 };
 
-Relax.geom.LengthConstraint.prototype.draw = function(ctxt, rc) {
+Relax.geom.LengthConstraint.prototype.draw = function(canvas, rc) {
+  var ctxt = canvas.ctxt;
   if (!rc.showConstraints) return;
 
   ctxt.lineWidth = 1;
@@ -307,10 +338,10 @@ Relax.geom.LengthConstraint.prototype.draw = function(ctxt, rc) {
 
   var angle = Math.atan2(this.p2.y - this.p1.y, this.p2.x - this.p1.x);
   var dist = 25;
-  var p1x = this.p1.x - dist * Math.cos(angle + Math.PI / 2);
-  var p1y = this.p1.y - dist * Math.sin(angle + Math.PI / 2);
-  var p2x = this.p2.x - dist * Math.cos(angle + Math.PI / 2);
-  var p2y = this.p2.y - dist * Math.sin(angle + Math.PI / 2);
+  var p1x = this.p1.x + canvas.offsetX - dist * Math.cos(angle + Math.PI / 2);
+  var p1y = this.p1.y + canvas.offsetY - dist * Math.sin(angle + Math.PI / 2);
+  var p2x = this.p2.x + canvas.offsetX - dist * Math.cos(angle + Math.PI / 2);
+  var p2y = this.p2.y + canvas.offsetY - dist * Math.sin(angle + Math.PI / 2);
 
   var textCenterX = (p1x + p2x) / 2 - dist / 2 * Math.cos(angle + Math.PI / 2);
   var textCenterY = (p1y + p2y) / 2 - dist / 2 * Math.sin(angle + Math.PI / 2);
@@ -350,7 +381,7 @@ RelaxCanvas.prototype.redraw = function() {
   this.ctxt.fillRect(0, 0, this.canvas.width, this.canvas.height);
   this.lines.forEach(function(l) { self.drawLine(l); });
   this.points.forEach(function(p) { self.drawPoint(p); });
-  this.relax.things.forEach(function(c) { if (c.draw) { c.draw(self.ctxt, self); } });
+  this.relax.things.forEach(function(c) { if (c.draw) { c.draw(self, self); } });
 };
 
 // -----------------------------------------------------
